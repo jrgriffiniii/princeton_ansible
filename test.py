4020bb906324a2b1e44e8ff7cad07e7d01faec70
GriffinJ <jrgriffiniii@gmail.com>
2019-08-06 10:18:56 -0400

    Working just with the Python variable

--- test.py
+++ test.py
@@ -10,10 +10,8 @@ for molecule_path_value in glob.glob('./roles/pulibrary.*/molecule'):
     role_path = Path(*molecule_path.parts[:-1])
     role_paths.append(str(role_path))
 os.environ['PULIBRARY_ROLES'] = "\n".join(role_paths)
-os.system('TESTFILES=$(circleci tests split --split-by=timings $PULIBRARY_ROLES)')
-test_files = os.system('echo $TESTFILES')
+test_files = os.system('circleci tests split --split-by=timings $PULIBRARY_ROLES')
 print(test_files)
-os.environ['TESTFILES'] = test_files
-for role in os.environ['TESTFILES']:
-    os.system(f'cd {role}')
+for role in test_files:
+    os.system(f'cd ~/princeton_ansible/{role}')
     os.system('molecule test && cd ..')
