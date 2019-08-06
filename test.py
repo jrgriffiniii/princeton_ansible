68240f00b286d1df71aa4c2e0ecd1eccac33e938
GriffinJ <jrgriffiniii@gmail.com>
2019-08-06 10:04:58 -0400

    Trying a different shell invocation

--- test.py
+++ test.py
@@ -10,8 +10,10 @@ for molecule_path_value in glob.glob('./roles/pulibrary.*/molecule'):
     role_path = Path(*molecule_path.parts[:-1])
     role_paths.append(str(role_path))
 os.environ['PULIBRARY_ROLES'] = "\n".join(role_paths)
-os.environ['TESTFILES'] = os.system('circleci tests split --split-by=timings $PULIBRARY_ROLES')
+os.system('TESTFILES=$(circleci tests split --split-by=timings $PULIBRARY_ROLES)')
+test_files = os.system('echo $TESTFILES')
+print(test_files)
+os.environ['TESTFILES'] = test_files
 for role in os.environ['TESTFILES']:
     os.system(f'cd {role}')
-    os.system('molecule test')
-    os.system('cd ..')
+    os.system('molecule test && cd ..')
